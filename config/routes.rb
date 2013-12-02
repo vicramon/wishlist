090132c9965b0da19ae4ff1039eece3cee8ed873
Vic Ramon <vic@vicramon.com>
2013-12-01 22:46:55 -0500

    Remove unnecessary brackets in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Wishlist::Application.routes.draw do
   resources :sessions, only: [:create, :destroy]
   resources :wishlists, only: [:index, :create, :update, :show]
   resources :comments, only: :create
-  resources :users, only: [:create]
+  resources :users, only: :create
 
   get '/sign_out', to: 'sessions#destroy', as: 'sign_out'
   get '/sign_in', to: 'home#index', as: 'sign_in'
